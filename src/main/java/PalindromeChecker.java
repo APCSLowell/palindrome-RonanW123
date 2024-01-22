45d195982729815d2b35ed294fc168a914b4b351
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-01-22 00:48:05 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,20 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  String sWord = new String();
+  for(int i = 0; i < word.length(); i++)
+    if(Character.isLetter(word.charAt(i)))
+      sWord += word.charAt(i);
+  sWord = sWord.toLowerCase();
+  if(sWord.equals(reverse(sWord)))
+    return true;
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+    for(int i = str.length()-1; i >= 0; i--)
+      sNew += str.charAt(i);
     return sNew;
 }
 }
